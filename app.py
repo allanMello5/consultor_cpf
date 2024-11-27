ff4c286b1c6d8338fde844c26611de3e1e3108c2
Allan Mello <89603616+allanMello5@users.noreply.github.com>
2024-11-27 18:26:36 -0300

    modified:   app.py 	new file:   planilha_fechamento.xlsx

--- app.py
+++ app.py
@@ -3,19 +3,21 @@ import openpyxl
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
+
 #1-Entrar na planilha e  extrair o cpf
 planilha_clientes = openpyxl.load_workbook('dados_clientes.xlsx')
 pagina_clientes = planilha_clientes['Sheet1']
 
+ # 2 entrar no site e usar o cpf para pesquisar o stuatus do pagamento daquele cliente. #2entro no site 10909090e9 para pesquisar o status do pagamento  daquele cliente
+driver = webdriver.Chrome()
+driver.get('http://localhost:52330/cpf.html')
+
 for linha in pagina_clientes.iter_rows(min_row=2, values_only=True):
-    nome, valor, cpf, vencimento = linha 
-    # 2 entrar no site e usar o cpf para pesquisar o stuatus do pagamento daquele cliente. #2entro no site 10909090e9 para pesquisar o status do pagamento  daquele cliente
-    driver = webdriver.Chrome()
-    driver.get('http://localhost:52330/cpf.html')
+    nome, valor, cpf, vencimento = linha  
     sleep(5)
     campo_pesquisa = driver.find_element(By.XPATH,"//input[@id='cpfInput']")
     sleep(1)
-    campo_pesquisa.send_keys(send_keys)
+    campo_pesquisa.send_keys(cpf)
     sleep(2)
     #vericar se esta em  dia ou atrasado
     botao_pesquisar = driver.find_element(By.XPATH,"//button[@class='']")
@@ -25,3 +27,9 @@ for linha in pagina_clientes.iter_rows(min_row=2, values_only=True):
     #4 se estiver em dia , pegar data do pagamento e o metodo de pagamento
     status = driver.find_element(By.XPATH,"//span[@id='statusLabel']")
     status.text
+    if status.text =='em dia':
+        data_pagamento = driver.find_element(By.XPATH"//p[@id='paymentDate']")
+        metodo_pagamento = driver.find_element(By.XPATH"//p[@id='paymentMehod']")
+    else:
+        planilha_fechamento = openpyxl.load_workbook('planilha fechamento.xlsx')
+        planilha_fechamento = planilha_fechamento['Planilha1']
